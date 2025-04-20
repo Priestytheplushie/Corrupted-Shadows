47264a7bf528b33cd5cfc9949f3ecefeb66297a1
Priesty <142067979+Priestytheplushie@users.noreply.github.com>
2025-04-20 19:02:19 -0400

    Player Class Setup

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Fuck you")
+# Priesty's Quest
+
+import time
+import random
+import cmd
+import sys
+import textwrap
+from colorama import Fore, Back, Style
+
+# Player Setup #
+
+class Player:
+    def __init__(self):
+        self.name = ""
+        self.hp = 0
+        self.strength = 0
+        self.speed = 0
+        self.intelligence = 0
+        self.defense = 0
+        self.money = 0
+        self.status_effects = []
+player = Player()
